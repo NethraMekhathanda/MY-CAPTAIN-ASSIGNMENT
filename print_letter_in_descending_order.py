36da58b9ab9c82376fce19720e47f2235028464d
NethraMekhathanda <121613580+NethraMekhathanda@users.noreply.github.com>
2022-12-31 10:12:46 +0530

    Create print_letter_in_descending_order.py

---
+++ print_letter_in_descending_order.py
@@ -0,0 +1,11 @@
+# python program to print a string in descending order
+# library function
+def descOrder(s):
+    s.sort(reverse=true)
+    str1=".join(s)"
+    print(str1)
+    def main():
+        s=list('mississippi')
+        #function call
+        descOrder(s)
+        if__name__=="main__"
